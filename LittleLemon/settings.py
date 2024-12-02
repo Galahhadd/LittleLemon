91e1bdf55e7c6aa9812f6b253fd8766e91173dc9
Anton <antonukr@icloud.com>
2024-12-02 15:07:39 +0200

    Added basic configuration

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #third party
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+
+    #my apps
+    'lemonapi.apps.LemonapiConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +129,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
